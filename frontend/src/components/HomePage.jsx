e28d15dfc278b7fc69b7cbe96beaf815a7d09611
Sujju-192 <2512surajdas@gmail.com>
2026-03-18 17:57:02 +0530

    Thode aur explaintions

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HomePage() {
   return (
     <div>
-      
+        {/* Is components mein left panel right panel aur wo panel ko call krna hai, jisme cards dikhenege scrollable hoga wo */}
     </div>
   )
 }
