4ff67406ddaed4a80c775fbc9cf65c98e9332d74
Sujju-192 <2512surajdas@gmail.com>
2026-03-18 17:55:11 +0530

    Thoda ur added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      
+      <>
+      {/* Is mein ek ek PAGE component call krna hai its the screen, smjh lo */}
+      {/* Page Components mein chhote choote components call krenge */}
+      </>
     </div>
   )
 }
